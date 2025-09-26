f1a6d0a82a803b3c81e17e5edad1967348699bc8
Ralph Olarte <olarteralphanthony@yahoo.com>
2025-09-26 09:47:12 -0400

    practice class

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 // import "../data/cart-class.js";
+import { Car } from "../data/car.js";
 
 renderOrderSummary();
 renderPaymentSummary();
