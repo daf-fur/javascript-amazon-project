c9d87f8bc5639302577d8a89e640b236aa5c45b9
daffur <olarteralphanthony@yahoo.com>
2025-10-07 13:58:27 -0400

    promises over callbacks

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,23 +1,27 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
+import {loadCart} from "../data/cart.js";
 // import "../data/cart-class.js";
 // import { Car } from "../data/car.js";
 // import '../data/backend-practice.js';
 
+/*
 new Promise((resolve) => {
-  console.log("start promise");
   loadProducts(() => {
-    console.log("finished loading");
     resolve();
   });
 }).then(() => {
-  console.log("next step");
-});
-
-/*
-loadProducts(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
 */
+
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+
+// 20:44:17
