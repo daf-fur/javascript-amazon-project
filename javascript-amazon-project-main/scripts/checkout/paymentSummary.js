8663b1acf11c8bffa097330a4f6633811284ecbc
Ralph Olarte <olarteralphanthony@yahoo.com>
2025-10-19 02:16:35 -0400

    finishing up place your order

--- javascript-amazon-project-main/scripts/checkout/paymentSummary.js
+++ javascript-amazon-project-main/scripts/checkout/paymentSummary.js
@@ -2,6 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js";
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrency } from "../utils/money.js";
+import { addOrder } from "../../data/orders.js";
 
 export function renderPaymentSummary() {
   let productPriceCents = 0;
@@ -66,17 +67,26 @@ export function renderPaymentSummary() {
 
   document.querySelector(".js-payment-summary").innerHTML = paymentSummaryHTML;
 
-  document.querySelector(".js-place-order").addEventListener("click", () => {
-    fetch("https:://supersimplebackend.dev/orders", {
-      method: "POST",
-      headers: {
-        "Content -Type": "application/json",
-      },
-      body: JSON.stringify({
-        cart: cart,
-      }),
+  document
+    .querySelector(".js-place-order")
+    .addEventListener("click", async () => {
+      try {
+        const response = await fetch("https://supersimplebackend.dev/orders", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            cart: cart,
+          }),
+        });
+
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log("Unexpected error. Try again later.");
+      }
     });
-  });
 }
 
-// 21:49:51
+// 22:00:45
